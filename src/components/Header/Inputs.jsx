f31fce1181e094f8eda40ce753ccf286521660d6
monsor3s <64220885+monsor3s@users.noreply.github.com>
2023-02-23 08:52:27 -0300

    adjusted elements in search form

--- src/components/Header/Inputs.jsx
+++ src/components/Header/Inputs.jsx
@@ -6,7 +6,7 @@ import "../../styles/components/inputs.sass"
 function Inputs() {
   return (
     <form action="" className="search-form">
-      <img src={SearchIcon} alt="" />
+      <img src={SearchIcon} alt="" className="search-icon" />
       <input type="text" className="input-text" placeholder="Search GitHub username"/>
       <input type="text" className="input-submit" value="Search"/>
     </form>
