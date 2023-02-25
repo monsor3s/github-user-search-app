6b99f864efaf7542bcfa310a43ae65c8173bf537
monsor3s <64220885+monsor3s@users.noreply.github.com>
2023-02-25 09:33:07 -0300

    create MainContainer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "../src/styles/components/app.sass"
 import "../src/styles/reset.sass"
 import "../src/styles/mixins.sass"
 import Header from "./components/Header/Header"
+import MainContainer from "./components/Body/MainContainer"
 
 
 function App() { 
 
   return (
+    <>
     <Header />
+    <MainContainer />
+    </>
   )
 }
 
---
+++ src/components/Body/MainContainer.jsx
@@ -0,0 +1,12 @@
+import "../../styles/components/main-container.sass"
+
+
+function MainContainer() {
+  return (
+    <main className="main-container">
+
+    </main>
+  )
+}
+
+export default MainContainer
