90bf08586122d854c78c40dc60024a108aa67d1b
monsor3s <64220885+monsor3s@users.noreply.github.com>
2023-03-01 08:47:57 -0300

    create stats container

--- src/components/Body/MainContainer.jsx
+++ src/components/Body/MainContainer.jsx
@@ -7,11 +7,25 @@ function MainContainer() {
     <main className="main-container">
       <div className="body-header">
           <img src={Logo} alt="Logo" className="logo-profile" />
-        <div className="main-header">
+        <header className="main-header">
           <h1 className="main-title">The Octocat</h1>
           <h2 className="main-date">Joined 25 Jan 2011</h2>
           <span className="username">@octocat</span>
           <span className="bio">This profile has no bio</span>
+        </header>
+        <div className="stats-container">
+          <div className="separador-div">
+            <h4 className="data-title">Repos</h4>
+            <p className="data-number">8</p>
+          </div>
+          <div className="separador-div">
+            <h4 className="data-title">Followers</h4>
+            <p className="data-number">3938</p>
+          </div>
+          <div className="separador-div">
+            <h4 className="data-title">Following</h4>
+            <p className="data-number">9</p>
+          </div>
         </div>
       </div>
     </main>
